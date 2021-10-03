8eafe7f418e29106515cf0426545cf40f4f8b76e
Rija1021 <rijach2019@namal.edu.pk>
2021-10-03 20:03:47 +0500

    Multiply Function

--- cal02.py
+++ cal02.py
@@ -1,6 +1,12 @@
 # Calculator in Python v01
+def sum(num1 ,num2):
+	return num1 + num2
 def sub(num1 , num2)
 	return num1 -num2
+def mul(a,b):
+	return a*b
+print(mul(8,5))
 
 # main
+print(add(5,3))
 print(sub(5,2))
