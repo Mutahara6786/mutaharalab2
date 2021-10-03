f6ec1b3df2b0ea65c447ad7134ca3fab7b10b088
Qaisarkang <qaisar2019@namal.edu.pk>
2021-10-03 19:23:20 +0500

    Sub function

--- cal01.py
+++ cal01.py
@@ -1,9 +1,13 @@
 # Calculator in Python v01
 def sum(num1 ,num2):
 	return num1 + num2
-def sub(num1 , num2)
-	return num1 -num2
 
 # main
 print(add(5,3))
-print(sub(5,2))
+
+
+
+def sub(num1 ,num2):
+	return num1 - num2
+print(sub(10,3))
+
