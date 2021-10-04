ccdd6e52a67c6820f38ff5832b846f1e3b060266
Moeez1021 <moeez2019@namal.edu.pk>
2021-10-04 17:46:13 +0500

    Division Function

---
+++ cal03.py
@@ -0,0 +1,15 @@
+# Calculator in Python v01
+def sum(num1 ,num2):
+	return num1 + num2
+def sub(num1 , num2)
+	return num1 -num2
+def mul(a,b):
+	return a*b
+print(mul(8,5))
+def div(a,b):
+	return a/b
+print(div(6,2))
+
+# main
+print(add(5,3))
+print(sub(5,2))
